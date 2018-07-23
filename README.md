27266fa1af6617bd17ed8f7e29a32ff8f0f29be4
奕盛 <yicheng@yishengdeMacBook-Pro.local>
2018-07-23 09:46:15 +0800

    初步完成本地mock平台的搭建，将需要的数据1⃣以json的格式放到soucrce中即可访问

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mockForSelf
 make data for self to check pro
+
+npm run test  //start pro
--- index.js
+++ index.js
@@ -1,37 +1,71 @@
 
 const koa = require('koa')
+const cors = require('koa2-cors')
 const server = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const router = require('koa-router')()
 const fs = require('fs')
 const publicPath = './source/'
 const app = new koa()
-app.use(server(__dirname+'/source/',{extensions:['.json']}))//eslint-disable-line
-app.use(bodyParser())
-app.use(router.routes())
 /* eslint-disable */
-router.get('/:name',async (ctx,next)=>{
+router.get('/',async (ctx,next)=>{
+    //ctx.query    
     let content = ''
     await new Promise((resolve,reject)=>{
-        fs.readFile(publicPath+ctx.params.name+'.json','utf8',(err,data)=>{
-            if(err)throw err
-            resolve(data);
+        fs.readFile(publicPath+ctx.query.name+'.json','utf8',(err,data)=>{
+              if(err)throw err
+              resolve(data)
         })
-    }).then(function(data){
+  }).then((data)=>{
+      content = data
+  })
+  ctx.body = content
+    ctx.body = content
+})
+//不能将get设置为动态路由，否则会跨域错误
+// router.get('/:name',async (ctx,next)=>{
+//     //ctx.request.body
+//     let content = ''
+//     await new Promise((resolve,reject)=>{
+//           fs.readFile(publicPath+ctx.params.name+'.json','utf8',(err,data)=>{
+//                 if(err)throw err
+//                 resolve(data)
+//           })
+//     }).then((data)=>{
+//         content = data
+//     })
+//     ctx.body = content
+// })
+router.post('/:name',async (ctx,next)=>{
+    //ctx.request.body
+    let content = ''
+    await new Promise((resolve,reject)=>{
+          fs.readFile(publicPath+ctx.params.name+'.json','utf8',(err,data)=>{
+                if(err)throw err
+                resolve(data)
+          })
+    }).then((data)=>{
         content = data
     })
-    ctx.body=content
-})
-router.post('/api/:name',(ctx,next)=>{
-    
-})
-router.put('/api/:name',(ctx,next)=>{
-    
-})
-router.del('/api/:name',(ctx,next)=>{
-    
+    ctx.body = content
 })
 /* eslint-enable */
+app.use(server(__dirname+'/source/',{extensions:['.json']}))//eslint-disable-line
+app.use(bodyParser())
+app.use(cors({
+    origin: function(ctx) {
+        if (ctx.url === '/test') {
+            return false
+        }
+        return '*'
+    },
+    exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+    maxAge: 5,
+    credentials: false,
+    allowMethods: ['GET', 'POST', 'DELETE'],
+    allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}))
+app.use(router.routes())
 app.use(router.allowedMethods())
 app.listen(3000)
 
