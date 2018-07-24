cd9bcc666bf22ab672f29c4fc207ff40c05c1649
奕盛 <yicheng@yishengdeMacBook-Pro.local>
2018-07-24 17:24:33 +0800

    添加head方法

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const koa = require('koa')
 //const cors = require('koa2-cors')
-//const server = require('koa-static')
+const server = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const router = require('koa-router')()
 const fs = require('fs')
@@ -10,18 +10,15 @@ const app = new koa()
 /* eslint-disable */
 router.get('/',async (ctx,next)=>{
     //ctx.query    
-    let content = ''
-    console.log(ctx.query.name)
     await new Promise((resolve,reject)=>{
         fs.readFile(publicPath+ctx.query.name+'.json','utf8',(err,data)=>{
               if(err)throw err
               resolve(data)
         })
   }).then((data)=>{
-      console.log(data)
-      content = data
+    ctx.body = data
   })
-    ctx.body = content
+    
 })
 //不能将get设置为动态路由，否则会跨域错误
 // router.get('/:name',async (ctx,next)=>{
@@ -39,19 +36,40 @@ router.get('/',async (ctx,next)=>{
 // })
 router.post('/:name',async (ctx,next)=>{
     //ctx.request.body
-    let content = ''
     await new Promise((resolve,reject)=>{
           fs.readFile(publicPath+ctx.params.name+'.json','utf8',(err,data)=>{
                 if(err)throw err
                 resolve(data)
           })
     }).then((data)=>{
-        content = data
+        ctx.body = data
     })
-    ctx.body = content
+   
+})
+/**
+ * 1.检查资源的有效性。 
+ * 2.检查超链接的有效性。 
+ * 3.检查网页是否被串改。 
+ * 4.多用于自动搜索机器人获取网页的标志信息，获取rss种子信息，或者传递安全认证信息等    
+ */
+router.head('/:name',async (ctx,next)=>{
+      await new Promise((resolve,reject)=>{
+          fs.access(publicPath+ctx.params.name+'.png',fs.constants.F_OK,(err)=>{
+              if(err){
+                  reject()
+                }else{
+                    resolve()
+                }
+
+          })
+      }).then((data)=>{
+          ctx.status = 200
+      }).catch((err)=>{
+          ctx.status=404
+      })
 })
 /* eslint-enable */
-//app.use(server('./source'))//eslint-disable-line
+app.use(server('./source'))//eslint-disable-line
 app.use(bodyParser())
 // app.use(cors({
 //     origin: function(ctx) {
@@ -69,13 +87,13 @@ app.use(bodyParser())
 app.use(async (ctx,next)=>{
     ctx.set('Access-Control-Allow-Origin','*')
     ctx.set('Access-Control-Max-Age',172800)
-    ctx.set('Access-Control-Allow-Methods','GET,POST,DELETE,OPTIONS')//options触发时校验服务器端支持的方法
+    ctx.set('Access-Control-Allow-Methods','GET,POST,DELETE,OPTIONS,PUT,HEAD')//options触发时校验服务器端支持的方法
     ctx.set('Access-Control-Allow-Headers','Content-Type,Authorization,Accept')//服务器端支持的头部信息字段
     ctx.set('Access-Control-Expose-Headers','WWW-Authenticate,Server-Authorization')
     await next()
 })
 app.use(router.routes())
-app.use(router.allowedMethods())
+app.use(router.allowedMethods())//主要是针对options方法进行处理
 app.listen(3000)
 
 
