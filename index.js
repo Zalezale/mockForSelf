16deeb97dc0f39be2ee31decfa54499b3bfe062f
奕盛 <yicheng@yishengdeMacBook-Pro.local>
2018-07-25 13:20:17 +0800

    添加put方法，与post做比较

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const fs = require('fs')
 const publicPath = './source/'
 const app = new koa()
 /* eslint-disable */
-router.get('/',async (ctx,next)=>{
+router.get('/', async (ctx, next) => {
     //ctx.query    
-    await new Promise((resolve,reject)=>{
-        fs.readFile(publicPath+ctx.query.name+'.json','utf8',(err,data)=>{
-              if(err)throw err
-              resolve(data)
+    await new Promise((resolve, reject) => {
+        fs.readFile(publicPath + ctx.query.name + '.json', 'utf8', (err, data) => {
+            if (err) throw err
+            resolve(data)
         })
-  }).then((data)=>{
-    ctx.body = data
-  })
-    
+    }).then((data) => {
+        ctx.body = data
+    })
+
 })
 //不能将get设置为动态路由，否则会跨域错误
 // router.get('/:name',async (ctx,next)=>{
@@ -34,17 +34,17 @@ router.get('/',async (ctx,next)=>{
 //     })
 //     ctx.body = content
 // })
-router.post('/:name',async (ctx,next)=>{
+router.post('/:name', async (ctx, next) => {
     //ctx.request.body
-    await new Promise((resolve,reject)=>{
-          fs.readFile(publicPath+ctx.params.name+'.json','utf8',(err,data)=>{
-                if(err)throw err
-                resolve(data)
-          })
-    }).then((data)=>{
+    await new Promise((resolve, reject) => {
+        fs.readFile(publicPath + ctx.params.name + '.json', 'utf8', (err, data) => {
+            if (err) throw err
+            resolve(data)
+        })
+    }).then((data) => {
         ctx.body = data
     })
-   
+
 })
 /**
  * 1.检查资源的有效性。 
@@ -52,21 +52,39 @@ router.post('/:name',async (ctx,next)=>{
  * 3.检查网页是否被串改。 
  * 4.多用于自动搜索机器人获取网页的标志信息，获取rss种子信息，或者传递安全认证信息等    
  */
-router.head('/:name',async (ctx,next)=>{
-      await new Promise((resolve,reject)=>{
-          fs.access(publicPath+ctx.params.name+'.png',fs.constants.F_OK,(err)=>{
-              if(err){
-                  reject()
-                }else{
-                    resolve()
-                }
-
-          })
-      }).then((data)=>{
-          ctx.status = 200
-      }).catch((err)=>{
-          ctx.status=404
-      })
+router.head('/:name', async (ctx, next) => {
+    await new Promise((resolve, reject) => {
+        fs.access(publicPath + ctx.params.name + '.png', fs.constants.F_OK, (err) => {
+            if (err) {
+                reject()
+            } else {
+                resolve()
+            }
+        })
+    }).then((data) => {
+        ctx.status = 200
+    }).catch((err) => {
+        ctx.status = 404
+    })
+})
+/**
+ * 幂等
+ * 创建成功201  更新成功200
+ * 响应体无
+ */
+router.put('/:name', async (ctx, next) => {
+    await new Promise((resolve, reject) => {
+        fs.writeFile(publicPath + ctx.params.name + '.txt', JSON.stringify(ctx.request.body), (err) => {
+            if (err){
+                reject(err)
+            } else{
+                resolve()
+            }
+        })
+    }).then(()=>{
+        ctx.set('Location', `${publicPath+ctx.params.name+'.txt'}`)
+        ctx.status = 201
+    })
 })
 /* eslint-enable */
 app.use(server('./source'))//eslint-disable-line
@@ -84,12 +102,12 @@ app.use(bodyParser())
 //     allowMethods: ['GET', 'POST', 'DELETE'],
 //     allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
 // }))
-app.use(async (ctx,next)=>{
-    ctx.set('Access-Control-Allow-Origin','*')
-    ctx.set('Access-Control-Max-Age',172800)
-    ctx.set('Access-Control-Allow-Methods','GET,POST,DELETE,OPTIONS,PUT,HEAD')//options触发时校验服务器端支持的方法
-    ctx.set('Access-Control-Allow-Headers','Content-Type,Authorization,Accept')//服务器端支持的头部信息字段
-    ctx.set('Access-Control-Expose-Headers','WWW-Authenticate,Server-Authorization')
+app.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*')
+    ctx.set('Access-Control-Max-Age', 172800)
+    ctx.set('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,HEAD')//options触发时校验服务器端支持的方法
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept')//服务器端支持的头部信息字段
+    ctx.set('Access-Control-Expose-Headers', 'WWW-Authenticate,Server-Authorization')
     await next()
 })
 app.use(router.routes())
